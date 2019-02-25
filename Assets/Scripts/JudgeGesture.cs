08516a2c069dafbf61de994ce317d728a32fcb87
Leo.wu <772618203@qq.com>
2019-02-25 19:36:52 +0800

    JudgeGseture和UpOrDown

---
+++ Assets/Scripts/JudgeGesture.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JudgeGesture : MonoBehaviour {
+
+    private Vector3 startPosition;
+    private Vector3 nowPosition;
+    private float yMoveDistance;
+    private bool isSelected = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount <= 0)
+        {
+            isSelected = false;
+            return;
+        }
+        if (Input.touchCount >= 0)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                
+            }
+        }
+
+        if (Input.GetTouch(0).phase == TouchPhase.Began)
+            startPosition = Input.GetTouch(0).position;
+        nowPosition = Input.GetTouch(0).position;
+        yMoveDistance = (nowPosition - startPosition).y;
+        print(yMoveDistance);
+ 
+    }
+}
---
+++ Assets/Scripts/UpOrDown.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpOrDown : MonoBehaviour {
+
+    private BeadUp beadUp;
+    private BeadDown beadDown;
+    public int number;
+
+    public enum State
+    {
+        up,
+        down
+    }
+    public State state;
+
+    // Use this for initialization
+    void Start () {
+        
+        switch (state)
+        {
+            case State.up:
+                beadUp = transform.parent.GetComponent<BeadUp>();
+                break;
+            case State.down:
+                beadDown = transform.GetComponent<BeadDown>();
+                break;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public void SendUp()
+    {
+        switch (state)
+        {
+            case State.up:
+                beadUp.Up();
+                break;
+            case State.down:
+                beadDown.Up(number);
+                break;
+        }
+    }
+
+    public void SendDown()
+    {
+        switch (state)
+        {
+            case State.up:
+                beadUp.Down();
+                break;
+            case State.down:
+                beadDown.Down(number);
+                break;
+        }
+    }
+}
